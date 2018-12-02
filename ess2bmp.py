49197727a15c8aa70b917c3c2febce6333488318
dylan <dknox2@my.westga.edu>
2018-12-01 21:59:51 -0500

    Increased readibility by removing magic numbers. Added usage text to be displayed when arguments are not inputted correctly.

--- ess2bmp.py
+++ ess2bmp.py
@@ -2,24 +2,49 @@
 
 import sys
 
-def generate_image():
-	if not args_are_valid():
-		sys.exit("Enter valid arguments")
+usage = """python3 tes5file.ess output.bmp
+./ess2bmp.py tes5file.ess output.bmp
+
+Writes the screenshot data from the given Skyrim save file to the given bitmap
+image file. Returns exit status 0 on a success, and non-zero otherwise (if the
+parameters are invalid).
+"""
+
+INCORRECT_USAGE = -1
+BMP_GENERATION_SUCCESS = 0
+
+TESV_MAGIC_OFFSET = 13
+TESV_HEADER_LENGTH = 17
+
+BMP_FILE_HEADER_LENGTH = 14
+BMP_IMAGE_HEADER_LENGTH = 40
+
+BMP_PIXEL_DATA_OFFSET = BMP_FILE_HEADER_LENGTH + BMP_IMAGE_HEADER_LENGTH
+
+UINT8 = 1
+UINT16 = 2
+UINT32 = 4
+
+def main():
+	validate_usage()
 
 	save_file_name = sys.argv[1]
 	with open(save_file_name, "rb") as save_file:
 		dimensions = find_shot_dimensions(save_file)
-
 		shot_data = find_shot_data(save_file, dimensions)
 
 	bmp_file_name = sys.argv[2]
-	with open(bmp_file_name, "w+b") as bmp_file:
-		write_bmp_file_header(bmp_file)
-		write_bmp_image_header(bmp_file, dimensions)
-		write_bmp_shot_data(bmp_file, shot_data)
+	write_bmp_file(bmp_file_name, dimensions, shot_data)
 
 	print("Shot width: {}".format(dimensions[0]))
 	print("Shot height: {}".format(dimensions[1]))
+
+	sys.exit(BMP_GENERATION_SUCCESS)
+
+def validate_usage():
+	if not args_are_valid():
+		print(usage)
+		sys.exit(INCORRECT_USAGE)
 
 def args_are_valid():
 	if (len(sys.argv)) != 3:
@@ -37,21 +62,20 @@ def find_shot_dimensions(save_file):
 	shot_size_offset = find_shot_size_offset(save_file)
 	save_file.seek(shot_size_offset)
 
-	shot_width = int.from_bytes(save_file.read(4), byteorder = "little")
-	shot_height = int.from_bytes(save_file.read(4), byteorder = "little")
+	shot_width = int.from_bytes(save_file.read(UINT32), byteorder = "little")
+	shot_height = int.from_bytes(save_file.read(UINT32), byteorder = "little")
 
 	return (shot_width, shot_height)
 
 def find_shot_size_offset(save_file):
-	# subtract 8 because the shot dimensions are two 4-byte integers
-	return find_shot_data_offset(save_file) - 8
+	return find_shot_data_offset(save_file) - UINT32 * 2
 
 def find_shot_data_offset(save_file):
-	save_file.seek(13)
-	header_size = int.from_bytes(save_file.read(4), byteorder = "little")
+	save_file.seek(TESV_MAGIC_OFFSET)
 
-	# add 17 bytes to get to the end header
-	return header_size + 17
+	header_size = int.from_bytes(save_file.read(UINT32), byteorder = "little")
+
+	return header_size + TESV_MAGIC_OFFSET + UINT32
 
 def find_shot_data(save_file, dimensions):
 	save_file.seek(find_shot_data_offset(save_file))
@@ -61,6 +85,7 @@ def find_shot_data(save_file, dimensions):
 		row = find_shot_data_row(save_file, dimensions[0], x)
 		data.append(row)
 
+	data.reverse()
 	return data
 
 def find_shot_data_row(save_file, row_width, row_num):
@@ -68,49 +93,48 @@ def find_shot_data_row(save_file, row_width, row_num):
 
 	row = list()
 	for x in range(row_width):
-		b = int.from_bytes(save_file.read(1), byteorder = "little")
-		g = int.from_bytes(save_file.read(1), byteorder = "little")
-		r = int.from_bytes(save_file.read(1), byteorder = "little")
+		b = int.from_bytes(save_file.read(UINT8), byteorder = "little")
+		g = int.from_bytes(save_file.read(UINT8), byteorder = "little")
+		r = int.from_bytes(save_file.read(UINT8), byteorder = "little")
 
 		row.append((r, g, b))
 
 	return row
 
+def write_bmp_file(file_name, dimensions, shot_data):
+	with open(file_name, "w+b") as bmp_file:
+		write_bmp_file_header(bmp_file)
+		write_bmp_image_header(bmp_file, dimensions)
+		write_bmp_shot_data(bmp_file, shot_data)
+
 def write_bmp_file_header(bmp_file):
 	bmp_file.write(b"BM")
-
-	# TODO write file size to file header (4 byte value)
-	bmp_file.write((0).to_bytes(4, byteorder = "little"))
-
+	bmp_file.write((0).to_bytes(UINT32, byteorder = "little"))
 	for x in range(2):
-		bmp_file.write((0).to_bytes(2, byteorder = "little"))
-
-	# pixel data offset: 14 for file header + 40 for image header
-	bmp_file.write((54).to_bytes(2, byteorder = "little"))
+		bmp_file.write((0).to_bytes(UINT16, byteorder = "little"))
+	bmp_file.write((BMP_PIXEL_DATA_OFFSET).to_bytes(UINT16, byteorder = "little"))
 
 def write_bmp_image_header(bmp_file, dimensions):
-	bmp_file.seek(14)
+	bmp_file.seek(BMP_FILE_HEADER_LENGTH)
 
-	bmp_file.write((40).to_bytes(4, byteorder = "little"))
+	bmp_file.write((BMP_IMAGE_HEADER_LENGTH).to_bytes(UINT32, byteorder = "little"))
 
 	for dimension in dimensions:
-		bmp_file.write((dimension).to_bytes(4, byteorder = "little"))
+		bmp_file.write((dimension).to_bytes(UINT32, byteorder = "little"))
 
-	bmp_file.write((1).to_bytes(2, byteorder = "little"))
-	bmp_file.write((24).to_bytes(2, byteorder = "little"))
+	bmp_file.write((1).to_bytes(UINT16, byteorder = "little"))
+	bmp_file.write((24).to_bytes(UINT16, byteorder = "little"))
 
-	# luckily everything past this point is optional and can be set to 0
 	for x in range(6):
-		bmp_file.write((0).to_bytes(4, byteorder = "little"))
+		bmp_file.write((0).to_bytes(UINT32, byteorder = "little"))
 
 def write_bmp_shot_data(bmp_file, shot_data):
-	bmp_file.seek(54)
+	bmp_file.seek(BMP_PIXEL_DATA_OFFSET)
 
-	shot_data.reverse()
 	for row in shot_data:
 		for pixel in row:
-			for color_value in pixel:
-				bmp_file.write((color_value).to_bytes(1, byteorder = "little"))
+			for UINT8_value in pixel:
+				bmp_file.write((UINT8_value).to_bytes(UINT8, byteorder = "little"))
 
 if __name__ == "__main__":
-	generate_image()
+	main()
